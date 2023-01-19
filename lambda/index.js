3d88a9a786e30c2921e5a886c353af0fbcd81205
Alexandre Alvaro <alexandre.alvaro@hotmail.com>
2023-01-19 15:30:23 -0300

    corrigindo arquivo ultrapassado

--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,8 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
 const Alexa = require('ask-sdk-core');
 const { Configuration, OpenAIApi } = require('openai');
 const keys = require('./keys');
@@ -8,13 +13,63 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
+const DOCUMENT_ID = "resposta-visual";
+
+const datasource = {
+    "headlineTemplateData": {
+        "type": "object",
+        "objectId": "headlineSample",
+        "properties": {
+            "backgroundImage": {
+                "contentDescription": null,
+                "smallSourceUrl": null,
+                "largeSourceUrl": null,
+                "sources": [
+                    {
+                        "url": "https://inteligenciadascoisas.com/post/20221209-t%c3%a1-assustado-com-as-evolu%c3%a7%c3%b5es-das-ias-olha-ent%c3%a3o-esse-chat-gtp/capa.jpg",
+                        "size": "large"
+                    }
+                ]
+            },
+            "textContent": {
+                "primaryText": {
+                    "type": "PlainText",
+                    "text": "texto"
+                }
+            },
+            "logoUrl": "https://openaichatgpt.com.br/wp-content/uploads/2022/12/cxv1.png",
+            "hintText": "Tente dizer, \"Pergunta, qual o comando de Linux para desligar o PC?\""
+        }
+    }
+};
+
+const createDirectivePayload = (aplDocumentId, dataSources = {}, tokenId = "documentToken") => {
+    return {
+        type: "Alexa.Presentation.APL.RenderDocument",
+        token: tokenId,
+        document: {
+            type: "Link",
+            src: "doc://alexa/apl/documents/" + aplDocumentId
+        },
+        datasources: dataSources
+    }
+};
+
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Bem vindo ao chat avançado com a "Open ei ai"! Para interagir, diga a palavra chave: "pergunta" seguida de sua pergunta!';
-
+        const speakOutput = 'Bem vindo ao chat avançado com a "Open ei ai"! Para interagir diga a palavra chave "pergunta", seguida de sua pergunta!';
+        
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            datasource.headlineTemplateData.properties.textContent.primaryText.text = "Bem vindo ao Modo Avançado."
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+        
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
@@ -42,7 +97,18 @@ const AskOpenAIIntentHandler = {
         });
 
         const speakOutput = response.data.choices[0].text +
-            'Gostaria de perguntar mais alguma coisa?';
+            '.\nGostaria de perguntar mais alguma coisa?';
+            
+        if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+            
+            datasource.headlineTemplateData.properties.textContent.primaryText.text = response.data.choices[0].text
+            
+            // generate the APL RenderDocument directive that will be returned from your skill
+            const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
+            // add the RenderDocument directive to the responseBuilder
+            handlerInput.responseBuilder.addDirective(aplDirective);
+        }
+
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -50,7 +116,6 @@ const AskOpenAIIntentHandler = {
             .getResponse();
     }
 };
-
 
 const HelpIntentHandler = {
     canHandle(handlerInput) {
@@ -81,7 +146,6 @@ const CancelAndStopIntentHandler = {
             .getResponse();
     }
 };
-
 /* *
  * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
  * It must also be defined in the language model (if the locale supports it)
@@ -101,7 +165,6 @@ const FallbackIntentHandler = {
             .getResponse();
     }
 };
-
 /* *
  * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
  * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
@@ -117,7 +180,6 @@ const SessionEndedRequestHandler = {
         return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
     }
 };
-
 /* *
  * The intent reflector is used for interaction model testing and debugging.
  * It will simply repeat the intent the user said. You can create custom handlers for your intents 
@@ -137,7 +199,6 @@ const IntentReflectorHandler = {
             .getResponse();
     }
 };
-
 /**
  * Generic error handling to capture any syntax or routing errors. If you receive an error
  * stating the request handler chain is not found, you have not implemented a handler for
